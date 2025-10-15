d89d35d14b5c184f3db9eef7bbfe2439457f4902
[dyad] <git@dyad.sh>
2025-10-15 15:39:00 -0300

    [dyad] Atualizando o link no componente "Made with Dyad". - wrote 1 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -1,14 +1,19 @@
+import React from 'react';
+
 export const MadeWithDyad = () => {
   return (
-    <div className="p-4 text-center">
-      <a
-        href="https://www.dyad.sh/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
-      >
-        Made with Dyad
-      </a>
+    <div className="p-4 text-center text-gray-500 text-sm">
+      <p>
+        Desenvolvido por{' '}
+        <a
+          href="https://www.thefastdeal.com.br/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:underline"
+        >
+          thefastdeal.com.br
+        </a>
+      </p>
     </div>
   );
 };
