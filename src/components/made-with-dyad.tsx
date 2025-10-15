8497a79c930e85c2019cdfd874af7722b3fa46f9
[dyad] <git@dyad.sh>
2025-10-15 15:42:56 -0300

    [dyad] Removendo o texto "Desenvolvido por" do componente MadeWithDyad. - wrote 1 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -4,14 +4,13 @@ export const MadeWithDyad = () => {
   return (
     <div className="p-4 text-center text-gray-500 text-sm">
       <p>
-        Desenvolvido por{' '}
         <a
           href="https://www.thefastdeal.com.br/"
           target="_blank"
           rel="noopener noreferrer"
           className="text-blue-600 hover:underline"
         >
-          thefastdeal.com.br
+          www.thefastdeal.com.br
         </a>
       </p>
     </div>
